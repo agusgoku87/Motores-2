d12bd4d2b1a2d0d59888511f3c5404c680f91422
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-10-29 20:42:24 -0300

    Scripts

---
+++ Assets/Scripts/Editor/Tile/TileEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Tile))]
+public class TileEditor : Editor
+{
+    private Tile tgt;
+
+
+    private void OnEnable()
+    {
+        tgt = (Tile)target;
+    }
+
+    private void OnSceneGUI()
+    {
+        Handles.BeginGUI();
+        Handles.EndGUI();
+    }
+
+    private void DrawButton(string text, Vector3 pos, Vector3 dir)
+    {
+        var _pos = Camera.current.WorldToScreenPoint(pos);
+        var size = 200 / Vector3.Distance(Camera.current.transform.position, pos);
+        var rect = new Rect(pos.x - size / 2, Screen.height - pos.y - size, size, size /2);
+
+        if (GUI.Button(rect, text))
+        {
+            Tile t = new Tile();
+            switch(PrefabWindow.prefab)
+            {
+                case "PuertaDoble":
+                    t = (Tile)Resources.Load("Prefabs/PuertaDoble", typeof(Tile));
+                    break;
+                case "PuertaDobleAbierta":
+                    t = (Tile)Resources.Load("Prefabs/PuertaDobleAbierta", typeof(Tile));
+                    break;
+                case "Arco":
+                    t = (Tile)Resources.Load("Prefabs/Arco", typeof(Tile));
+                    break;
+                case "Piso":
+                    t = (Tile)Resources.Load("Prefabs/Piso", typeof(Tile));
+                    break;
+                case "ParedConcava":
+                    t = (Tile)Resources.Load("Prefabs/ParedConcava", typeof(Tile));
+                    break;
+                case "ParedConvexa":
+                    t = (Tile)Resources.Load("Prefabs/ParedConvexa", typeof(Tile));
+                    break;
+                case "Pared":
+                    t = (Tile)Resources.Load("Prefabs/Pared", typeof(Tile));
+                    break;
+            }
+            Tile _tile = Instantiate(t);
+            t.transform.forward = (dir - tgt.transform.position).normalized;
+            t.transform.position = dir + (_tile.transform.forward.normalized * 3f);
+            Selection.activeObject = t;
+            SceneView.lastActiveSceneView.LookAt(t.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum TileName
+    {
+        PuertaDoble,
+        PuertaDobleAbierta,
+        Arco,
+        Piso,
+        ParedConcava,
+        ParedConvexa,
+        Pared,
+        Fin
+    }
+
+    //public List<string> TileNames = new List<string>() 
+    //{ "PuertaDoble", "PuertaDobleAbierta", "Arco", "Piso", "ParedConcava", "ParedConvexa", "Pared" };
+
+    public TileName currentTileName
+    {
+        get
+        {
+            var gameObjectName = gameObject.name;
+            if (gameObjectName.Contains("PuertaDoble")) return TileName.PuertaDoble;
+            if (gameObjectName.Contains("PuertaDobleAbierta")) return TileName.PuertaDobleAbierta;
+            if (gameObjectName.Contains("Arco")) return TileName.Arco;
+            if (gameObjectName.Contains("Piso")) return TileName.Piso;
+            if (gameObjectName.Contains("ParedConvexa")) return TileName.ParedConvexa;
+            if (gameObjectName.Contains("ParedConcava")) return TileName.ParedConcava;
+            else return TileName.Pared;
+
+        }
+    }
+}
