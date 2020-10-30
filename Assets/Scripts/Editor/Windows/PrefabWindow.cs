591a090f7b21a56d099f284609d7368fc02a6606
Akuraaa <72999443+Akuraaa@users.noreply.github.com>
2020-10-30 00:36:38 -0300

    Scriptscitospa

--- Assets/Scripts/Editor/Windows/PrefabWindow.cs
+++ Assets/Scripts/Editor/Windows/PrefabWindow.cs
@@ -7,7 +7,7 @@ public class PrefabWindow : EditorWindow
 {
     //List<string> prefabName = new List<string>(); No pudimos pasarle el index en el popUp, por eso usamos array, pero no es la idea que buscamos.
     //public List<string> pName = new List<string>() { "A", "B", "C", "D" };
-    string[] prefabName = { "PuertaDoble", "PuertaDobleAbierta", "Arco", "Piso", "ParedConcava", "ParedConvexa", "Pared"};
+    string[] prefabName = {"Pared", "Final", "Cruce", "ParedConcava", "PisoTecho", "PuertaAbierta"};
     public static string prefab;
     int prefabIndex;
     Vector2 _scrollPos;
@@ -24,7 +24,8 @@ public class PrefabWindow : EditorWindow
         _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, false, false);
         prefabIndex = EditorGUILayout.Popup("Prefab", prefabIndex, prefabName);
         prefab = prefabName[prefabIndex];
-
+        Repaint();
         EditorGUILayout.EndVertical();
+
     }
 }
